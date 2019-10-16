7a2398bde95b3c9e154e645f78484578add3e7c6
Siarhei Lunski <the.sergaunt@gmail.com>
2019-10-16 21:22:59 +0300

    Add rush monorepo

---
+++ apps/vscode/src/test/suite/index.js
@@ -0,0 +1,44 @@
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var path = __importStar(require("path"));
+var Mocha = __importStar(require("mocha"));
+var glob = __importStar(require("glob"));
+function run() {
+    // Create the mocha test
+    var mocha = new Mocha({
+        ui: 'tdd',
+    });
+    mocha.useColors(true);
+    var testsRoot = path.resolve(__dirname, '..');
+    return new Promise(function (c, e) {
+        glob('**/**.test.js', { cwd: testsRoot }, function (err, files) {
+            if (err) {
+                return e(err);
+            }
+            // Add files to the test suite
+            files.forEach(function (f) { return mocha.addFile(path.resolve(testsRoot, f)); });
+            try {
+                // Run the mocha test
+                mocha.run(function (failures) {
+                    if (failures > 0) {
+                        e(new Error(failures + " tests failed."));
+                    }
+                    else {
+                        c();
+                    }
+                });
+            }
+            catch (err) {
+                e(err);
+            }
+        });
+    });
+}
+exports.run = run;
