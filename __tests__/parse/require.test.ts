9e9efddb6c5df47746ca3fffbaf27db8855eec4d
Siarhei Lunski <the.sergaunt@gmail.com>
2019-11-24 20:55:18 +0300

    Minor enhancements

--- __tests__/parse/require.test.ts
+++ __tests__/parse/require.test.ts
@@ -1,33 +1,33 @@
 import { _require } from '../../src/parser';
 
 describe('require statement', () => {
-  it('rqr>package', () => {
+  it('require statement', () => {
     expect(_require('>package')).toEqual("const package = require('package');");
   });
 
-  it('rqr>customName>package', () => {
+  it('require with custom name', () => {
     expect(_require('>customName>package')).toEqual(
       "const customName = require('package');",
     );
   });
 
-  it('rqr:package', () => {
+  it('with object desctructuring', () => {
     expect(_require(':package')).toEqual("const {} = require('package');");
   });
 
-  it('rqr:obj>package', () => {
+  it('with object desctructuring 1 parameter', () => {
     expect(_require(':obj>package')).toEqual(
       "const { obj } = require('package');",
     );
   });
 
-  it('rqr:objA,objB>package', () => {
+  it('with object desctructuring 2 parameters', () => {
     expect(_require(':objA,objB>package')).toEqual(
       "const { objA, objB } = require('package');",
     );
   });
 
-  it('rqr:objA,objB,objC>package', () => {
+  it('with object desctructuring 3 parameters', () => {
     expect(_require(':objA,objB,objC>package')).toEqual(
       "const { objA, objB, objC } = require('package');",
     );
